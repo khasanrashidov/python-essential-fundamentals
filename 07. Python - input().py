131f2503c56be09f60c72cea6341ad3aa495ca87
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-08-24 13:57:17 +0500

    Add files via upload

---
+++ 07. Python - input().py
@@ -0,0 +1,11 @@
+print() #empty line
+##input()
+##input('Press any key to proceed')
+##input('The end of the program')
+
+age = input('Enter your age : ')
+age = int(age) 
+#or just using :
+#age = int(input('Enter your age : '))
+separator = '*' * 50
+print(separator)

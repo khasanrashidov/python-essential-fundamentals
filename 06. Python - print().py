8bb62c07ae6baff3677cd79059f4a6964b5c4118
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-08-24 13:57:06 +0500

    Add files via upload

---
+++ 06. Python - print().py
@@ -0,0 +1,37 @@
+print() # empty line
+print('Hello, Python')
+print("Hello, Python")
+print('H'+'e'+'l'+'l'+'o'+','+' '+'Python')
+print('''Triple quotes
+are used for
+multiline outputs''')
+print('='*30)
+print('''At our disposal 4 types of quotes:
+"""
+We can use
+- 'apostrophes'
+- "double" quotes
+- two types of triple quotes
+"""
+''')
+print('='*30)
+a=100
+print(a)
+print(a+1)
+print('_'*30)
+print('a =', a, 'and', 'a + 1 =', a+1)
+print('='*30)
+print('Hello', end=' ')
+print('Python', 'Java', 'C++', sep=', ')
+print() # empty line
+print('='*30)
+# Экранирование - Escape Characters
+# \\ - backslash. Output: \
+# \" - quote. Output: "
+# \' - apostrophe. Output: '
+# \t - tab
+# \n - new line
+x = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 +\
+    1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 +\
+    1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+print(x)

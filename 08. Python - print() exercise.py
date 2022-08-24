a9cf938cfcbfbd1b91685e5a9cdb98fb4f2be977
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-08-24 13:57:29 +0500

    Add files via upload

---
+++ 08. Python - print() exercise.py
@@ -0,0 +1,16 @@
+print() #empty line
+print('1'+'0'*128)
+print('='*30)
+a=10
+b=15
+if a+b==25:
+    c=30
+if c==30:
+    print("Excellent")
+
+
+x, y, z = 5, 3.2, "Hello"
+
+print (x)
+print (y)
+print (z)

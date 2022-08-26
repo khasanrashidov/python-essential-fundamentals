f067641bc758bf6f9e5a2e9011e8adaf29a1cfdb
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-08-26 22:27:50 +0500

    Add files via upload

---
+++ 15. Python - for loop, while loop vs for loop.py
@@ -0,0 +1,17 @@
+print()
+print('while loop')
+n = 0
+while n < 5:
+    print(n)
+    n = n + 1
+
+print()
+print('_' * 50, '\n\nfor loop')
+for i in range(5):
+    print(i)
+
+## range()
+## range(start, stop, step)
+## be default start = 0, and step = 1
+## range(5): 0 1 2 3 4
+## range(2, 10, 3): 2, 5, 8

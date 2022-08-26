dd59bdf016bba43ad2b39e2606638eef8bcc7900
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-08-26 22:26:52 +0500

    Add files via upload

---
+++ 13. Python - while loop part 1.py
@@ -0,0 +1,20 @@
+print()
+
+n = 0 ## n should be defined
+while n < 5:
+    print(n)
+    n += 1
+
+## if condition is Flase (or not fulfilled),
+## then instructions in cycle are not performed
+##
+## infinite loop heats the computer
+
+n = 0
+while n < 5:
+    print(n)
+
+## to exit infinite loop press Ctrl+C
+
+##############################
+

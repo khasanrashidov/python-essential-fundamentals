26e3f38e39c13d24fe68aa7f9adef46665df5a0e
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-09-04 18:44:47 +0500

    Add files via upload

---
+++ 28. Python - string - accessing elements by index - direct access.py
@@ -0,0 +1,8 @@
+## accessing elements by index - direct access
+print()
+word = 'Python'
+for i in range(len(word)):
+    print('Index', i, 'Symbol', word[i])
+print()
+print('#' * 50)
+##################################################

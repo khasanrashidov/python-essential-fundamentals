668e596b665bc4326e3e7e639501418b86982ae0
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-09-05 20:59:29 +0500

    Add files via upload

---
+++ 32. Python - tuples, partition.py
@@ -0,0 +1,18 @@
+## tuples
+
+print('#' * 50)
+print()
+
+email = 'info@pylab.ru'
+print(email)
+
+print(email.partition('@'))
+name, at, domain = email.partition('@')
+
+print(name)
+print(at)
+print(domain)
+
+print('#' * 50)
+print()
+##################################################

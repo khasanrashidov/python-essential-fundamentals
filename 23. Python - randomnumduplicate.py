0db621fbba38b0e391b2c0434eade758adaa9e30
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-09-10 08:34:04 +0500

    Add files via upload

---
+++ 23. Python - randomnumduplicate.py
@@ -0,0 +1,12 @@
+import random
+
+# duplicates are allowed
+num1 = random.randint(0, 40)
+num2 = random.randint(0, 40)
+num3 = random.randint(0, 40)
+num4 = random.randint(0, 40)
+
+print("randomnum1 =", num1)
+print("randomnum2 =", num2)
+print("randomnum3 =", num3)
+print("randomnum4 =", num4)
